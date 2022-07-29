3db6fd91737cb039bc88bfc3846b7da2d1cbca2e
rshanson711 <rshanson711@gmail.com>
2022-07-29 01:35:55 -0500

    Further work on function-based PlayerSearch
    PlayerSearchNew now has basic functionality

--- src/components/PlayerSearchNew.js
+++ src/components/PlayerSearchNew.js
@@ -1,27 +1,29 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
-export default async function PlayerSearchNew(props) {
-    //const data = await fetch("http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code='mlb'&active_sw='Y'&name_part='" + props.name + "%25'");
-    const data = await fetch("http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code='mlb'&active_sw='Y'&name_part='judge%25'");
-    //get player image
-
-    let playerData;
+export default function PlayerSearchNew(props) {
+    const [playerData, setPlayerData] = useState(null);
     const [name, setName] = useState(null);
     const [origin, setOrigin] = useState(null);
     const [college, setCollege] = useState(null);
-    
-    //CONVERT DATA TO JSON
-    try {
-        playerData = await data.json();
-    } catch {
-        console.log("Failed to read var: playerData");
-    }
 
-    setName(playerData.search_player_all.queryResults.row.name_use + " " + playerData.search_player_all.queryResults.row.name_last);
-    setOrigin(playerData.search_player_all.queryResults.row.birth_city + ", " + playerData.search_player_all.queryResults.row.birth_state);
-    setCollege(playerData.search_player_all.queryResults.row.college);
+    useEffect(() => {
+        fetch("http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code='mlb'&active_sw='Y'&name_part='" + props.name + "%25'")
+            .then(response => response.json())
+            .then(data => setPlayerData(data));
 
-    console.log("hello");
+        //fetch("http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code='mlb'&active_sw='Y'&name_part='judge%25'")
+        //get player image 
+    }, [props.name]);
+
+    useEffect(() => {
+        if (playerData != null) {
+            setName(playerData.search_player_all.queryResults.row.name_use + " " + playerData.search_player_all.queryResults.row.name_last);
+            setOrigin(playerData.search_player_all.queryResults.row.birth_city + ", " + playerData.search_player_all.queryResults.row.birth_state);
+            setCollege(playerData.search_player_all.queryResults.row.college);
+        }
+    }, [playerData]);
+
+    console.log("Line 31: playerData === " + playerData);
     return (
         <div className='playerContainer'>
             <h1>{name}</h1>
